00b6d5754bb452761bb77b37d46382018127a171
munna-68 <mahamudmunna68@gmail.com>
2026-01-07 20:41:48 +0600

    working on the menu part of the site

---
+++ src/js/home.js
@@ -0,0 +1,9 @@
+const rightDiv = document.querySelector('.right div');
+const rightDivContainer = document.querySelector('.right-div-container');
+
+
+
+export renderHome() {
+  rightDiv.innerHTML = '';
+  
+}
